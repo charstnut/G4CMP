a22661a4ecf671ffd51f60ebcc9c013fd7ec905f
Michael Kelsey <kelsey@slac.stanford.edu>
2023-05-27 15:48:47 -0700

    Remove all rate model/MFP comparisons from TimeStepper.  Just set maximum step length from config.

--- library/include/G4CMPTimeStepper.hh
+++ library/include/G4CMPTimeStepper.hh
@@ -17,6 +17,8 @@
 // 20200426  G4CMP-196: Change "impact" name to "trapIon"
 // 20220730  G4CMP-301: Drop trapping processes, as they have built-in MFPs,
 //		don't need TimeStepper for energy-dependent calculation.
+// 20230527  Drop competitive MFP calculation; use this process to enforce
+//	       a maximum allowed step length, to support mass recalculation.
 
 #ifndef G4CMPTimeStepper_h
 #define G4CMPTimeStepper_h 1
@@ -24,16 +26,10 @@
 #include "globals.hh"
 #include "G4CMPVDriftProcess.hh"
 
-class G4CMPVScatteringRate;
-
-
 class G4CMPTimeStepper : public G4CMPVDriftProcess {
 public:
   G4CMPTimeStepper();
   virtual ~G4CMPTimeStepper();
-
-  // Initialize local pointers to Luke and IV scattering rate models
-  virtual void LoadDataForTrack(const G4Track* aTrack);
 
   // No random throw here: MFP and GPIL are fixed lengths
   virtual G4double 
@@ -46,27 +42,8 @@ public:
   virtual G4VParticleChange* PostStepDoIt(const G4Track& aTrack,
 					  const G4Step& aStep);
 
-  // Allow external process to supply rate model
-  void UseLukeRateModel(const G4CMPVScatteringRate* aRate) { lukeRate = aRate; }
-  void UseIVRateModel(const G4CMPVScatteringRate* aRate)   { ivRate = aRate; }
-
 protected:  
   virtual G4double GetMeanFreePath(const G4Track&,G4double,G4ForceCondition*);
-
-  // Maximum rate for other processes, given track kinematics
-  G4double MaxRate(const G4Track& aTrack) const;
-
-  // Step length in E-field needed to reach specified energy
-  G4double DistanceToThreshold(const G4CMPVScatteringRate* rate,
-			       G4double Estart) const;
-  G4double EnergyStep(G4double Estart, G4double Efinal) const;
-
-  // Get scattering rates for other charge-carrier processes
-  void ReportRates(const G4Track& aTrack);
-
-  // Pointers may be changed from Use functions
-  const G4CMPVScatteringRate* lukeRate;
-  const G4CMPVScatteringRate* ivRate;
 
 private:
   //hide assignment operator
--- library/src/G4CMPTimeStepper.cc
+++ library/src/G4CMPTimeStepper.cc
@@ -39,26 +39,22 @@
 //		be delta(E)/(q*V).
 // 20220730  Drop trapping processes, as they have built-in MFPs, and don't
 //		need TimeStepper for energy-dependent calculation.
+// 20230527  Drop competitive MFP calculation; use this process to enforce
+//	       a maximum allowed step length, to support mass recalculation.
 
 #include "G4CMPTimeStepper.hh"
 #include "G4CMPConfigManager.hh"
-#include "G4CMPDriftElectron.hh"
-#include "G4CMPDriftHole.hh"
 #include "G4CMPDriftTrackInfo.hh"
 #include "G4CMPFieldUtils.hh"
 #include "G4CMPGeometryUtils.hh"
 #include "G4CMPTrackUtils.hh"
 #include "G4CMPUtils.hh"
-#include "G4CMPVProcess.hh"
-#include "G4CMPVScatteringRate.hh"
 #include "G4DynamicParticle.hh"
 #include "G4Field.hh"
 #include "G4FieldManager.hh"
 #include "G4LatticePhysical.hh"
 #include "G4LogicalVolume.hh"
 #include "G4PhysicalConstants.hh"
-#include "G4ProcessManager.hh"
-#include "G4ProcessVector.hh"
 #include "G4SystemOfUnits.hh"
 #include "G4Track.hh"
 #include "G4UserLimits.hh"
@@ -67,93 +63,19 @@
 #include <math.h>
 
 G4CMPTimeStepper::G4CMPTimeStepper()
-  : G4CMPVDriftProcess("G4CMPTimeStepper", fTimeStepper), lukeRate(nullptr),
-    ivRate(nullptr) {;}
+  : G4CMPVDriftProcess("G4CMPTimeStepper", fTimeStepper) {;}
 
 G4CMPTimeStepper::~G4CMPTimeStepper() {;}
 
 
-// Get scattering rates from current track's processes
-
-void G4CMPTimeStepper::LoadDataForTrack(const G4Track* aTrack) {
-  G4CMPProcessUtils::LoadDataForTrack(aTrack);	// Common configuration
-
-  // Get rate model for Luke phonon emission from process
-  const G4CMPVProcess* lukeProc =
-    dynamic_cast<G4CMPVProcess*>(G4CMP::FindProcess(aTrack,
-						    "G4CMPLukeScattering"));
-  lukeRate = lukeProc ? lukeProc->GetRateModel() : nullptr;
-  if (lukeRate)
-    const_cast<G4CMPVScatteringRate*>(lukeRate)->LoadDataForTrack(aTrack);
-
-  // get rate model for intervalley scattering from process
-  const G4CMPVProcess* ivProc =
-    dynamic_cast<G4CMPVProcess*>(G4CMP::FindProcess(aTrack,
-					    "G4CMPInterValleyScattering"));
-  ivRate = ivProc ? ivProc->GetRateModel() : nullptr;
-  if (ivRate) 
-    const_cast<G4CMPVScatteringRate*>(ivRate)->LoadDataForTrack(aTrack);
-
-  if (verboseLevel>1) {
-    G4cout << "TimeStepper Found" 
-	   << (lukeRate?" lukeRate":"")
-	   << (ivRate?" ivRate":"") 
-	   << G4endl;
-  }
-
-  // Adjust rate models to keep highest verbosity level
-  if (lukeRate && lukeRate->GetVerboseLevel() < verboseLevel)
-    const_cast<G4CMPVScatteringRate*>(lukeRate)->SetVerboseLevel(verboseLevel);
-
-  if (ivRate && ivRate->GetVerboseLevel() < verboseLevel)
-    const_cast<G4CMPVScatteringRate*>(ivRate)->SetVerboseLevel(verboseLevel);
-}
-
-
-// Compute fixed "minimum distance" to avoid accelerating past Luke or IV
+// Return configured maximum step length, or DBL_MAX if not set
 
 G4double G4CMPTimeStepper::GetMeanFreePath(const G4Track& aTrack, G4double,
 					   G4ForceCondition* cond) {
-  if (verboseLevel == -1) ReportRates(aTrack);	// SPECIAL FLAG TO REPORT
-
   *cond = NotForced;
 
-  // SPECIAL:  If no electric field, no need to limit steps
-  if (G4CMP::GetFieldAtPosition(aTrack).mag() <= 0.) return DBL_MAX;
-
-  // Evaluate different step lengths to avoid overrunning process thresholds
-  G4double vtrk = GetVelocity(aTrack);
-  G4double ekin = GetKineticEnergy(aTrack);
-
-  // Get step length due to fastest process
-  G4double rate = MaxRate(aTrack);
-  G4double mfpFast = rate>0. ? vtrk/rate : DBL_MAX;
-
-  if (verboseLevel>1) {
-    G4cout << "TS Vtrk " << vtrk/(m/s) << " m/s mfp0 " << mfpFast/m << " m"
-	   << G4endl;
-  }
-
-
-  // Find distance to Luke threshold
-  G4double mfpLuke = DistanceToThreshold(lukeRate, ekin);
-  if (verboseLevel>1)
-    G4cout << "TS Luke threshold mfpLuke " << mfpLuke/m << " m" << G4endl;
-
-  // Find distance to IV scattering threshold 
-  G4double mfpIV = DistanceToThreshold(ivRate, ekin);
-  if (verboseLevel>1)
-    G4cout << "TS IV threshold mfpIV " << mfpIV/m << " m" << G4endl;
-
-  // Take shortest distance from above options
-  G4double mfp = std::min({mfpFast, mfpLuke, mfpIV});
-
-  if (verboseLevel) {
-    G4cout << GetProcessName() << (IsElectron()?" elec":" hole")
-	   << " MFP = " << mfp/m << " m" << G4endl;
-  }
-
-  return mfp;
+  G4double maxStep = G4CMPConfigManager::GetMaximumStep();
+  return (maxStep>0. ? maxStep : DBL_MAX);
 }
 
 
@@ -169,83 +91,4 @@ G4VParticleChange* G4CMPTimeStepper::PostStepDoIt(const G4Track& aTrack,
 
   ClearNumberOfInteractionLengthLeft();		// All processes must do this!
   return &aParticleChange;
-}
-
-
-// Get maximum rate for other processes at given kinematics
-
-G4double G4CMPTimeStepper::MaxRate(const G4Track& aTrack) const {
-  G4double lrate = lukeRate ? lukeRate->Rate(aTrack) : 0.;
-  G4double irate = ivRate ? ivRate->Rate(aTrack) : 0.;
-
-  if (verboseLevel>2) {
-    G4cout << "G4CMPTimeStepper::MaxRate luke " << lrate/hertz << " iv "
-	   << irate/hertz << " Hz" << G4endl;
-  }
-
-  return std::max(lrate,irate);
-}
-
-
-// Get distance due to energy gain to threshold (step) in rate
-
-G4double G4CMPTimeStepper::DistanceToThreshold(const G4CMPVScatteringRate* rate,
-					       G4double Estart) const {
-  if (!rate) return DBL_MAX;		// Skip if no rate model
-
-  // Avoid taking "too short" steps, which causes "stuck tracks"
-  G4double MINstep = G4CMPConfigManager::GetMinStepScale();
-  MINstep *= (IsElectron() ? theLattice->GetElectronScatter()
-		: theLattice->GetHoleScatter());
-
-  if (MINstep<0) MINstep = 1e-6*m;
-
-  return std::max(EnergyStep(Estart, rate->Threshold(Estart)), MINstep);
-}
-
-// Get step length in E-field needed to reach specified energy
-
-G4double G4CMPTimeStepper::EnergyStep(G4double Estart, G4double Efinal) const {
-  if (Estart > Efinal) return DBL_MAX;		// Already over threshold
-
-  const G4Track* trk = GetCurrentTrack();
-
-  G4double EMmag = G4CMP::GetFieldAtPosition(*trk).mag();
-  if (EMmag <= 0.) return DBL_MAX;		// No field, no acceleration
-
-  if (verboseLevel>1) {
-    G4cout << "G4CMPTimeStepper::EnergyStep from " << Estart/eV
-	   << " to " << Efinal/eV << " eV : " 
-	   << (Efinal-Estart)/(eplus*EMmag) << " mm" << G4endl;
-  }
-
-  // Add 20% rescaling to account for electron valley systematics
-  return 1.2*(Efinal-Estart)/(eplus*EMmag);
-}
-
-
-// Report Luke and IV rates for diagnostics
-
-void G4CMPTimeStepper::ReportRates(const G4Track& aTrack) {
-  static G4ThreadLocal G4bool first = true;
-  if (first) {
-    G4cout << "TSreport Process Chg E[eV] v[m/s] k[1/m] Rate[Hz]" << G4endl;
-    first = false;
-  }
-  
-  G4cout << "TSreport Luke"
-	 << " " << aTrack.GetDynamicParticle()->GetCharge()/eplus
-	 << " " << GetKineticEnergy(aTrack)/eV
-	 << " " << GetVelocity(aTrack)/(m/s)
-	 << " " << GetLocalWaveVector(aTrack).mag()*m
-	 << " " << lukeRate->Rate(aTrack)/hertz << G4endl;
-  
-  if (IsElectron()) {
-    G4cout << "TSreport IV"
-	   << " " << aTrack.GetDynamicParticle()->GetCharge()/eplus
-	   << " " << GetKineticEnergy(aTrack)/eV
-	   << " " << GetVelocity(aTrack)/(m/s)
-	   << " " << GetLocalWaveVector(aTrack).mag()*m
-	   << " " << ivRate->Rate(aTrack)/hertz << G4endl;
-  }      
 }
